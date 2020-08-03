171186fb5d3bc864f1112356a9baf0fd3ec4b2c2
Cagdas <cagdascaglak@gmail.com>
2020-08-03 21:28:53 +0300

    Add framework and create singleton class

---
+++ platform/iphone/lib/ReflectionFramework.framework/Headers/ReflectionFramework.h
@@ -0,0 +1,20 @@
+//
+//  ReflectionFramework.h
+//  ReflectionFramework
+//
+//  Created by Çağdaş Çağlak on 27.07.2020.
+//  Copyright © 2020 Çağdaş Çağlak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for ReflectionFramework.
+FOUNDATION_EXPORT double ReflectionFrameworkVersionNumber;
+
+//! Project version string for ReflectionFramework.
+FOUNDATION_EXPORT const unsigned char ReflectionFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReflectionFramework/PublicHeader.h>
+
+#import "ReflectionFramework/ReflectionFrameworkCls.h"
---
+++ platform/iphone/lib/ReflectionFramework.framework/Headers/ReflectionFrameworkCls.h
@@ -0,0 +1,17 @@
+//
+//  ReflectionFrameworkCls.h
+//  ReflectionFramework
+//
+//  Created by Çağdaş Çağlak on 27.07.2020.
+//  Copyright © 2020 Çağdaş Çağlak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ReflectionFrameworkCls : NSObject
+-(void)callMeFunc;
+@end
+
+NS_ASSUME_NONNULL_END
